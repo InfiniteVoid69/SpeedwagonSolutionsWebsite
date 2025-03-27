57a30e13b505249b2603cb487197013e71c3b1db
InfiniteVoid <ADJordan305@hotmail.com>
2025-03-27 18:38:51 -0500

    formatting

--- src/pages/api/formHandler.ts
+++ src/pages/api/formHandler.ts
@@ -1,5 +1,5 @@
 export const prerender = false;
-import { Resend } from 'resend';
+import { Resend } from "resend";
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY);
 const timestamp = new Date().toLocaleString();
@@ -10,25 +10,28 @@ export async function POST(context: { request: Request }) {
 
     let htmlBody = `<h2>New Form Submission: ${timestamp} </h2>`;
     for (const [key, value] of Object.entries(formData)) {
-      htmlBody += `<p><strong>${key.replace(/([A-Z])/g, ' $1')}:</strong> ${value}</p>`;
+      htmlBody += `<p><strong>${key.replace(/([A-Z])/g, " $1")}:</strong> ${value}</p>`;
     }
 
     await resend.emails.send({
-      from: 'Speedwagon Contact <forms@speedwagonsolutions.com>',
-      to: ['contact@speedwagonsolutions.com'],
-      subject: 'Form Submission',
+      from: "Speedwagon Contact <forms@speedwagonsolutions.com>",
+      to: ["contact@speedwagonsolutions.com"],
+      subject: "Form Submission",
       html: htmlBody,
     });
 
-    return new Response(JSON.stringify({ message: 'Email sent successfully' }), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return new Response(
+      JSON.stringify({ message: "Email sent successfully" }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   } catch (err) {
     console.error(err);
-    return new Response(JSON.stringify({ error: 'Failed to send email' }), {
+    return new Response(JSON.stringify({ error: "Failed to send email" }), {
       status: 500,
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
   }
 }
