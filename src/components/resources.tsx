93609fe6217500b7decd6a04bde14d5547cedee8
InfiniteVoid <ADJordan305@hotmail.com>
2025-04-02 11:55:19 -0500

    fixes

--- src/components/resources.tsx
+++ src/components/resources.tsx
@@ -1,5 +1,36 @@
 import { FaInstagram, FaGoogle, FaYelp } from "react-icons/fa";
 
+export const services = [
+  {
+    id: "residential-washing",
+    name: "Residential",
+    photo: "/images/RESIDENTIAL.jpg",
+    info: "Residential pressure washing services are designed to clean and maintain the exterior surfaces of homes, including houses, driveways, walkways, roofs, decks, and patios.",
+    tags: ["Houses", "Driveways & Walkways", "Roofs", "Decks & Patios"],
+  },
+  {
+    id: "commercial-washing",
+    name: "Commercial",
+    photo: "/images/COMMERCIAL.jpg",
+    info: "Commercial pressure washing services are designed to clean and maintain the exterior surfaces of businesses, ensuring a professional appearance.",
+    tags: ["Fast Food", "Restaurants", "Property Management"],
+  },
+  {
+    id: "window-washing",
+    name: "Window Washing",
+    photo: "/images/WINDOW.jpg",
+    info: "Window washing services involve the cleaning of glass surfaces in residential and commercial buildings, ensuring clear visibility and enhancing the overall appearance.",
+    tags: ["Exterior Windows", "Interior Windows", "Glass Doors"],
+  },
+  // {
+  //   id: "line-Striping",
+  //   name: "Line Striping",
+  //   photo: "/images/",
+  //   info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //   tags: ["Parking Lots", "Asphalt", "Concrete", "Warehouses"],
+  // },
+];
+
 export const navItems = [
   {
     id: 1,
@@ -10,6 +41,11 @@ export const navItems = [
     id: 2,
     name: "Services",
     link: "/services",
+    sublinks: services.map((service) => ({
+      id: service.id,
+      name: service.name,
+      link: `/services/${service.id}`,
+    })),
   },
   {
     id: 3,
@@ -52,35 +88,4 @@ export const socialMedia = [
     link: "https://www.yelp.com/biz/speedwagon-solutions-white-settlement/",
     icon: <FaYelp />,
   },
-];
-
-export const services = [
-  {
-    id: "residential-washing",
-    name: "Residential",
-    photo: "/images/RESIDENTIAL.jpg",
-    info: "Residential pressure washing services are designed to clean and maintain the exterior surfaces of homes, including houses, driveways, walkways, roofs, decks, and patios.",
-    tags: ["Houses", "Driveways & Walkways", "Roofs", "Decks & Patios"],
-  },
-  {
-    id: "commercial-washing",
-    name: "Commercial",
-    photo: "/images/COMMERCIAL.jpg",
-    info: "Commercial pressure washing services are designed to clean and maintain the exterior surfaces of businesses, ensuring a professional appearance.",
-    tags: ["Fast Food", "Restaurants", "Property Management"],
-  },
-  {
-    id: "window-washing",
-    name: "Window Washing",
-    photo: "/images/WINDOW.jpg",
-    info: "Window washing services involve the cleaning of glass surfaces in residential and commercial buildings, ensuring clear visibility and enhancing the overall appearance.",
-    tags: ["Exterior Windows", "Interior Windows", "Glass Doors"],
-  },
-  // {
-  //   id: "line-Striping",
-  //   name: "Line Striping",
-  //   photo: "/images/",
-  //   info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  //   tags: ["Parking Lots", "Asphalt", "Concrete", "Warehouses"],
-  // },
 ];
