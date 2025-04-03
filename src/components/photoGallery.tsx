113bd736dfce6dcbd48a1c6240ad7d14937e6efd
InfiniteVoid <ADJordan305@hotmail.com>
2025-04-03 15:22:10 -0500

    title added

--- src/components/photoGallery.tsx
+++ src/components/photoGallery.tsx
@@ -13,40 +13,43 @@ const imageList = [
 
 const PhotoGallery = () => {
   return (
-    <Swiper
-      spaceBetween={10}
-      slidesPerView="auto"
-      scrollbar={{ draggable: true }}
-      modules={[Scrollbar, Autoplay, Pagination]}
-      style={{ padding: "10px", backgroundColor: "#1a1a1a" }}
-      loop={true}
-      autoplay={{ delay: 2500, disableOnInteraction: false }}
-    >
-      {imageList.map((image, index) => (
-        <SwiperSlide key={index} style={{ width: "500px" }}>
-          <div style={{ position: "relative", color: "#fff" }}>
-            <img
-              src={image}
-              alt={`gallery-image-${index + 1}`}
-              style={{
-                width: "100%",
-                height: "200px",
-                objectFit: "cover",
-                borderRadius: "8px",
-              }}
-            />
-            <p
-              style={{
-                marginTop: "8px",
-                fontSize: "14px",
-                textAlign: "center",
-              }}
-            >
-            </p>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <h2 className="text-3xl font-bold text-center mb-12">Photo Gallery</h2>
+      <Swiper
+        spaceBetween={10}
+        slidesPerView="auto"
+        scrollbar={{ draggable: true }}
+        modules={[Scrollbar, Autoplay, Pagination]}
+        style={{ padding: "10px", backgroundColor: "#1a1a1a" }}
+        loop={true}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
+      >
+        {imageList.map((image, index) => (
+          <SwiperSlide key={index} style={{ width: "500px" }}>
+            <div style={{ position: "relative", color: "#fff" }}>
+              <img
+                src={image}
+                alt={`gallery-image-${index + 1}`}
+                style={{
+                  width: "100%",
+                  height: "200px",
+                  objectFit: "cover",
+                  borderRadius: "8px",
+                }}
+              />
+              <p
+                style={{
+                  marginTop: "8px",
+                  fontSize: "14px",
+                  textAlign: "center",
+                }}
+              >
+              </p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
 
